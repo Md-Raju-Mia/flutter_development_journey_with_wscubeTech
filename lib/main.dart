b2cfa335c7b77aa9924173ed350a7f46b0203728
Md.Raju Mia <70192276+Md-Raju-Mia@users.noreply.github.com>
2025-11-30 19:01:01 +0600

    positioned widget

--- lib/main.dart
+++ lib/main.dart
@@ -1670,7 +1670,7 @@ class _MyHomePageState extends State<MyHomePage> {
 ///Then click on the "Installing" copy the dependency for this page
 ///Add the copied dependency to the pubspec.yaml file
 ///Also copy the import package line form that page and paste it to your project file.
-void main(){
+/*void main(){
   runApp(MyApp());
 }
 
@@ -1724,6 +1724,76 @@ class _MyHomePageState extends State<MyHomePage> {
             ],
           )
       ),
+    );
+  }
+}*/
+
+
+
+/**---------------------------------------------------------------------*/
+
+
+///Positioned Widget
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      home: const MyHomePage(),
+    );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Hello Position Widget Icon",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 21,
+            fontWeight: FontWeight.w700,
+          ),),
+        backgroundColor: Colors.red,
+      ),
+
+     body: Container(
+       // width: double.infinity,
+       width: 300,
+       // height: double.infinity,
+       height: 200,
+       color: Colors.blueGrey,
+       child: Stack(
+         children: [
+           Positioned(
+             bottom: 41,
+             right: 41,
+             child: Container(
+               width: 100,
+               height: 100,
+               color: Colors.white,
+             ),
+           )
+         ],
+       ),
+     ),
     );
   }
 }
